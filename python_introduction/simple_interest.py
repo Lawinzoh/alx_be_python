3ee793a4dd22a79865234150992a19d64829d2eb
Lawrence <lawrenceowino96@gmail.com>
2025-07-11 19:48:12 -0700

    Updated simple_interest.py with new edits

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+# Calculate simple interest
+principal = 1000 #principal amount (initial investment)
+rate = 0.05 #annual interest rate
+time = 3 #time in years
+interest = principal * time * rate
+print("The simple interest is:", interest)
